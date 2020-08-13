353a97999250bf012d46455112168af3c40e7956
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-08-14 02:29:18 +0300

    Переводит простые (не требующие параметров) компоненты в классы

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import {createSiteMenuTemplate} from "./view/site-menu.js";
+import SiteMenuView from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createTaskTemplate} from "./view/task.js";
 import {createTaskEditTemplate} from "./view/task-edit.js";
-import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
-import {createBoardTemplate} from "./view/board.js";
+import LoadMoreButtonView from "./view/load-more-button.js";
+import BoardView from "./view/board.js";
+import SortView from "./view/sort.js";
+import TaskListView from "./view/task-list.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
-import {renderTemplate} from "./utils.js";
+import {renderTemplate, renderElement, RenderPosition} from "./utils.js";
 
 const TASK_COUNT = 22;
 const TASK_COUNT_PER_STEP = 8;
@@ -17,36 +19,40 @@ const filters = generateFilter(tasks);
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-renderTemplate(siteHeaderElement, createSiteMenuTemplate());
+renderElement(siteHeaderElement, new SiteMenuView().getElement());
 renderTemplate(siteMainElement, createFilterTemplate(filters));
-renderTemplate(siteMainElement, createBoardTemplate());
 
-const boardElement = siteMainElement.querySelector(`.board`);
-const taskListElement = boardElement.querySelector(`.board__tasks`);
+const boardComponent = new BoardView();
 
-renderTemplate(taskListElement, createTaskEditTemplate(tasks[0]));
+renderElement(siteMainElement, boardComponent.getElement());
+renderElement(boardComponent.getElement(), new SortView().getElement(), RenderPosition.AFTERBEGIN);
+
+const taskListComponent = new TaskListView();
+
+renderElement(boardComponent.getElement(), taskListComponent.getElement());
+renderTemplate(taskListComponent.getElement(), createTaskEditTemplate(tasks[0]));
 
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderTemplate(taskListElement, createTaskTemplate(tasks[i]));
+  renderTemplate(taskListComponent.getElement(), createTaskTemplate(tasks[i]));
 }
 
 if (tasks.length > TASK_COUNT_PER_STEP) {
   let renderedTaskCount = TASK_COUNT_PER_STEP;
 
-  renderTemplate(boardElement, createLoadMoreButtonTemplate());
+  const loadMoreButtonComponent = new LoadMoreButtonView();
+  renderElement(boardComponent.getElement(), loadMoreButtonComponent.getElement());
 
-  const loadMoreButton = boardElement.querySelector(`.load-more`);
-
-  loadMoreButton.addEventListener(`click`, (evt) => {
+  loadMoreButtonComponent.getElement().addEventListener(`click`, (evt) => {
     evt.preventDefault();
     tasks
       .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderTemplate(taskListElement, createTaskTemplate(task)));
+      .forEach((task) => renderTemplate(taskListComponent.getElement(), createTaskTemplate(task)));
 
     renderedTaskCount += TASK_COUNT_PER_STEP;
 
     if (renderedTaskCount >= tasks.length) {
-      loadMoreButton.remove();
+      loadMoreButtonComponent.getElement().remove();
+      loadMoreButtonComponent.removeElement();
     }
   });
 }
